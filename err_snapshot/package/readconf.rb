e460f6954bd32bb724d08376a4f1497b23e1dab1
Slim <slim@ubuntu.(none)>
2015-10-04 15:09:00 -0700

    change if to case when for cmd_list

--- err_snapshot/package/readconf.rb
+++ err_snapshot/package/readconf.rb
@@ -15,24 +15,33 @@ class ReadConf < Conf
     iniparse[type.upcase].each do |item|
       conf_hash[item.key] = item.value
     end
+#if func is log, need to collect syslog also
     if func == "log" 
       iniparse["system"].each do |item|
         conf_hash[item.key] = item.value
       end
     end
+#according to the type to collect different items and add the items into a Hash
     return conf_hash
   end
 end
 
 class CollectLogs < ReadConf
   def collect_logs(func)
+#delete the last log files, if it exists
     File.delete("logs/#{func}.log") if File::exists?( "logs/#{func}.log" )
     get_conf("#{func}").each do | key ,value |
       if value != 0
         sysFile = File.new("logs/#{func}.log","a+")
-        cmd_list = ["lsof -c #{key}","ps aux|grep #{key}|grep -v grep"] if func == "process"
-        cmd_list = ["netstat -an | grep #{value}"] if func == "port"
-        cmd_list = ["tail -100 #{value}"] if func == "applog"
+	case func 
+	  when "process" then cmd_list = ["lsof -c #{key}","ps aux|grep #{key}|grep -v grep"]
+	  when "port"    then cmd_list = ["netstat -an | grep #{value}"]
+          when "applog"  then cmd_list = ["tail -100 #{value}"]
+	  else cmd_list = []
+	end
+        #cmd_list = ["lsof -c #{key}","ps aux|grep #{key}|grep -v grep"] if func == "process"
+        #cmd_list = ["netstat -an | grep #{value}"] if func == "port"
+        #cmd_list = ["tail -100 #{value}"] if func == "applog"
         cmd_list.each do |cmd|
           sysFile.syswrite(`#{cmd}`)
         end
