f5c7ff6144fbc2e356915933283bf9a9b10da2af
Slim <daichenge@gmail.com>
2015-10-06 21:26:43 +0800

    change logbak direcotry on s3

--- err_snapshot/testconf.rb
+++ err_snapshot/testconf.rb
@@ -2,16 +2,14 @@ require './package/readconf.rb'
 require './package/s3op.rb'
 require 'find'
 hostname = `hostname`
-=begin
+time = Time.now.strftime("%Y-%m-%d")
 dbconf = CollectLogs.new(hostname,"db")
 ["load", "process", "port", "applog"].each do |item|
   dbconf.collect_logs(item)
 end
-=end
 s3client = Aws::S3::Client.new(region: 'ap-southeast-1')
 ["load.log", "process.log", "port.log", "applog.log"].each do |item|
-  logbak = S3PutGet.new(s3client, 'slim-chef/err_snapshot/' + hostname, item, item, "logs")
+  buckname = 'slim-chef/err_snapshot/' + time + "/" + hostname
+  logbak = S3PutGet.new(s3client, buckname, item, item, "logs")
   logbak.put_s3()
 end
-#path = File.dirname(__FILE__)
-#puts path
