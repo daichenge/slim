ee536ae944c34c7363b60fe41f45e508c27e4fb4
Slim <daichenge@gmail.com>
2015-10-06 16:47:06 +0800

    add funciton upload to s3

--- err_snapshot/testconf.rb
+++ err_snapshot/testconf.rb
@@ -1,6 +1,17 @@
 require './package/readconf.rb'
+require './package/s3op.rb'
+require 'find'
 hostname = `hostname`
+=begin
 dbconf = CollectLogs.new(hostname,"db")
 ["load", "process", "port", "applog"].each do |item|
   dbconf.collect_logs(item)
 end
+=end
+s3client = Aws::S3::Client.new(region: 'ap-southeast-1')
+["load.log", "process.log", "port.log", "applog.log"].each do |item|
+  logbak = S3PutGet.new(s3client, 'slim-chef/err_snapshot/' + hostname, item, item, "logs")
+  logbak.put_s3()
+end
+#path = File.dirname(__FILE__)
+#puts path
